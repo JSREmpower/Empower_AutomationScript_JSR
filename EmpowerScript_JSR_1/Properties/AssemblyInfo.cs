e1376c79d86c8c717082ef6ad447858902be07ab
Jeeva Suria Rajah <jeeva.suria.rajah@skyline.be>
2023-06-03 23:35:09 +0800

    Add project files.

---
+++ EmpowerScript_JSR_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerScript_JSR_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerScript_JSR_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("FDF0B33B-167C-417D-9E9B-18B5632AAD82")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,1 @@
+# EmpowerScript_JSR
