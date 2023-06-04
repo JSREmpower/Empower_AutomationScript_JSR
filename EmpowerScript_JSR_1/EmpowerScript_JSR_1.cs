c4e1bfb2bc9789514534eac661dc19a57fcbaa4f
Jeeva Suria Rajah <jeeva.suria.rajah@skyline.be>
2023-06-04 11:49:51 +0800

    Second Update

--- EmpowerScript_JSR_1/EmpowerScript_JSR_1.cs
+++ EmpowerScript_JSR_1/EmpowerScript_JSR_1.cs
@@ -68,7 +68,7 @@ namespace EmpowerScript_JSR_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World, Jeeva");
+			engine.GenerateInformation("Hello World, Rajesh");
 		}
 	}
 }
